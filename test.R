f3fb3bbbb4a7a40a9cfb10b579a4bd5e58d6e070
Robert Hijmans <r.hijmans@gmail.com>
2025-11-13 06:42:35 -0800

    Update test.R

--- test.R
+++ test.R
@@ -2,7 +2,11 @@ library(terra)
 libVersion()
 gdal(warn = 1)
 
-url <- "/vsicurl/https://os.zhdk.cloud.switch.ch/chelsav2/GLOBAL/climatologies/1981-2010/bio/CHELSA_bio10_1981-2010_V.2.1.tif"
-e = ext(c(-26, 37.5, 34, 72))
-r = rast(url, win = e, raw = TRUE)
-r = toMemory(r)
+library(terra)
+r <- rast()
+cellFromXY(r, cbind(NA, NA))
+
+#url <- "/vsicurl/https://os.zhdk.cloud.switch.ch/chelsav2/GLOBAL/climatologies/1981-2010/bio/CHELSA_bio10_1981-2010_V.2.1.tif"
+#e = ext(c(-26, 37.5, 34, 72))
+#r = rast(url, win = e, raw = TRUE)
+#r = toMemory(r)
